471ae29eb0af022a2022228edd1faccd8266a857
Wilder Rincon <1588@holbertonschool.com>
2022-01-23 12:32:23 -0500

    adding changes file index.js routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
+
+const express = require('express');
 
 const Routers = (app) => {
   const router = express.Router();
